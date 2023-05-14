27525ad9d5d110bf3f70ed499ac0aeac38247310
Anisimov Artem <goldbergbill@icloud.com>
2023-05-14 22:01:47 +0300

    Добавлен класс Command

--- Homework_18_Patterns/Infrastructure/Commands/Base/Command.cs
+++ Homework_18_Patterns/Infrastructure/Commands/Base/Command.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Homework_18_Patterns.Infrastructure.Commands.Base
+﻿namespace Homework_18_Patterns.Infrastructure.Commands.Base
 {
-    class Command
+    internal class Command
     {
     }
 }
